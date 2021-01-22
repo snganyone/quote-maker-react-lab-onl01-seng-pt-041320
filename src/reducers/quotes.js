98df1282fc268d7cb56280014bb956b28be42c5b
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-01-21 18:57:54 -0800

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,13 @@
-export default (state = [], action) => {
-  return state;
+export default (state = {
+  quotes: [],
+}, action) => {
+  switch(action.type){
+    case 'ADD_QUOTE':
+      return { quotes: state.quotes.concat(action.payload.content)}
+
+
+    default:
+      return state;
+  }
+  
 }
