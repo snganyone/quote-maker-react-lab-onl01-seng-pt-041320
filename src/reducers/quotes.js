b42f9ff9017823a3487aea3e115af3f78dfc5ac5
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-01-22 16:39:04 -0800

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,8 +1,29 @@
 export default (state = [], action) => {
+  let newstate;
+
   switch(action.type){
     case 'ADD_QUOTE':
-      return { action }
-
+      return [ ...state, action.quote ]
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId)
+    case 'UPVOTE_QUOTE':
+      newstate = state.map(quote => {
+        if(quote.id === action.quoteId){
+          return {...quote, votes: ++quote.votes}
+        } else {
+          return quote
+        }
+      })
+      return newstate;
+    case 'DOWNVOTE_QUOTE':
+    newstate = state.map(quote => {
+      if(quote.id === action.quoteId && quote.votes > 0){
+        return {...quote, votes: --quote.votes}
+      } else {
+        return quote
+      }
+    })
+    return newstate;
     default:
       return state;
   }
